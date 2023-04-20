5d6f1615a8a5b2b1856b4cbd966fc448c9b46904
smmmfrd <smmmfrd@gmail.com>
2023-04-19 22:11:08 -0700

    Clicking Logic

--- Tic Tac Toe/game.gd
+++ Tic Tac Toe/game.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+@export var playerTurn = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,3 +8,5 @@ func _ready():
 
 func square_clicked(x, y):
 	print("clicked at (" + str(x) + ", " + str(y) + ").")
+	print(playerTurn)
+	playerTurn = not playerTurn
--- Tic Tac Toe/square.gd
+++ Tic Tac Toe/square.gd
@@ -1,11 +1,22 @@
 extends Sprite2D
 
-
 @export var x := 0
 @export var y := 0
+
+# The ole' Mumford Singleton
+@onready var game_manager = $"../..";
+var filled = false
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		if get_rect().has_point(to_local(event.position)):
-			# Call up dumbass.
-			$"../..".square_clicked(x, y);
+			if(not filled):
+				selected()
+
+func selected():
+	if(game_manager.playerTurn):
+		modulate = Color(0, 0, 1)
+	else:
+		modulate = Color(1, 0, 0)
+	filled = true
+	game_manager.square_clicked(x, y)
